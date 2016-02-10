773aab6163a1a1021600fc96dd5c2c1ddff0d047
Krishna Mohan R Bakka <bkmr17@yahoo.com>
2016-02-09 23:54:27 -0600

    Update versionb.sh

--- versionb.sh
+++ versionb.sh
@@ -4,8 +4,9 @@ filename=$(basename $1)
 #storing the file extension in variable extension
 extension="${filename##*.}"
 #storing the processed file name into another variable
-name="$(basename -s .txt $1)_$(date '+%m-%d-%y').$extension"
+name="${filename%.*}_$(date '+%m-%d-%y').$extension"
 #outputs the name to the terminal
 echo "File $name has been created"
 #generating the file and copying the file into new file
 cat $filename > $name
+
